bc0bc16eb60e4463a7a4512f9ec448fdcfa14f66
Maciej Kowalski <maciej.ko1444@gmail.com>
2023-08-29 12:18:24 +0200

    output deleted

--- main.tf
+++ main.tf
@@ -1,23 +1,6 @@
 provider "aws" {
   region = "eu-north-1"
 }
-resource "aws_instance" "example" {
-  ami = "ami-0989fb15ce71ba39e"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "Terraform-example"
-  }
-}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -29,6 +12,7 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 variable "server_port" {
   description = "the port the server will use for HTTP request"
   type = number
@@ -38,4 +22,42 @@ variable "server_port" {
 output "public_ip" {
   value = aws_instance.example.public_ip
   description = "The public IP address of the web server"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0989fb15ce71ba39e"
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag{
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnets" "default"{
+  filter{
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
